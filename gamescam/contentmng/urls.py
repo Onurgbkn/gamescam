28d1258d995e330b6a945735867288b394c90231
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2021-06-04 22:20:26 +0300

    Add templates

--- gamescam/contentmng/urls.py
+++ gamescam/contentmng/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'game'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:game_id>/', views.detail, name = 'detail')
--- gamescam/contentmng/views.py
+++ gamescam/contentmng/views.py
@@ -1,18 +1,27 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.http.response import Http404
 from .models import Game
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 
+    
 def index(request):
     latest_game_list = Game.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('contentmng/index.html')
-    context = {
-        'latest_game_list': latest_game_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_game_list': latest_game_list}
+    return render(request, 'contentmng/index.html', context)
+
 
 
 def detail(request, game_id):
-    return HttpResponse("You're in %s detail page." % game_id)
+    try:
+        game = Game.objects.get(pk=game_id)
+    except Game.DoesNotExist:
+        raise Http404("The game you looking for is not here.")
+    return render(request, 'contentmng/detail.html', {'game': game})
+
+    
+def detail(request, game_id):
+    game = get_object_or_404(Game, pk=game_id)
+    return render(request, 'contentmng/detail.html', {'game': game})
