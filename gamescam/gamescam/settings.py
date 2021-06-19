47463e856b101b594daa76e306f862490e04015e
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2021-06-19 22:36:04 +0300

    games app

--- gamescam/gamescam/settings.py
+++ gamescam/gamescam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config # For secret key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*4blpx!p#kf-0!jcs9qk(_!xju=$d2dmks8x^=m3%n)c*=4pk5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
