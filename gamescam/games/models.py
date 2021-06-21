c2b4f2371b4e14a3e5f668e9c3b559803776f6c0
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2021-06-21 22:27:23 +0300

    Redo

--- gamescam/games/models.py
+++ gamescam/games/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+
+class Game(models.Model):
+    game_name = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('date published')
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.game_name;
+
+
+class Comment(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.comment_text;
--- gamescam/games/views.py
+++ gamescam/games/views.py
@@ -1,3 +1,18 @@
+from django import template
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Game
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    latest_games = Game.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('games/index.html')
+    context = {
+        'latest_games': latest_games,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, game_id):
+    return HttpResponse("Detail page of %s" % game_id)
